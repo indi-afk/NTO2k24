c53efaeae7e80ca5ed2121b7a4a4fdf894b753aa
indi-afk <sergeymaloy365@gmail.com>
2024-12-10 14:06:26 +0300

    Update 3.1file.py

--- 3.1file.py
+++ 3.1file.py
@@ -7,6 +7,7 @@ import numpy as np
 from sensor_msgs.msg import Image, Range
 from cv_bridge import CvBridge
 from clover import long_callback
+from std_msgs.msg import String
 
 rospy.init_node('flight')
 
@@ -39,15 +40,22 @@ bridge = CvBridge()
 
 def find_color(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    red_lower = np.array([0, 70, 80])
-    red_upper = np.array([20,100,100])
-    blue_lower = np.array([165, 70, 70])
-    blue_upper = np.array([255,100,100])
-    green_lower = np.array([65, 70, 70])
-    green_upper = np.array([140,100,100])
-    yellow_lower = np.array([40, 70, 80])
-    yellow_upper = np.array([60,100,100])
     
+    #red = cv2.inRange(hsv, (0, 250, 250), (20,255,255))
+    #blue = cv2.inRange(hsv, (87, 250, 250), (95,255,255))
+    #green = cv2.inRange(hsv, (65, 250, 250), (80,255,255))
+    #yellow = cv2.inRange(hsv, (27,250,250), (35,255,255))
+
+
+    red_lower = np.array([0, 250, 250])
+    red_upper = np.array([20,255,255])
+    blue_lower = np.array([87, 250, 250])
+    blue_upper = np.array([95,255,255])
+    green_lower = np.array([50, 250, 250])
+    green_upper = np.array([85,255,255])
+    yellow_lower = np.array([27,250,250])
+    yellow_upper = np.array([35,255,255])
+
     color = []
     #red
     mask = cv2.inRange(hsv, red_lower, red_upper)
@@ -78,27 +86,33 @@ def image_callback(data):
     global col
     col=find_color(img)
     
-
+buildings_pub = rospy.Publisher('/buildings', String, queue_size=1)
 image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback)
 
 navigate_wait(frame_id = 'body', auto_arm=True)
 xd=0
-for f in range(0,10):
-    while (xd != 10 and f % 2 == 0) or (xd != -1 and f % 2 == 1):
+for f in range(0,16):
+    while (xd < 10 and f % 2 == 0) or (xd > -1 and f % 2 == 1):
         if 0.7 < range1 < 1.3:
             rospy.sleep(3)
             telemetry = get_telemetry()
             x1 = telemetry.x
             y1 = telemetry.y
-            print(x1, y1, col)
-                
+            print(x1, y1, col[0])
+            x2 = str(x1)
+            y2 = str(y1)
+            fraza = 'x = ' + x2 +', y = ' + y2 + ', color = ' + col[0]
+            buildings_pub.publish(data = fraza)
+
+                     
         if f % 2 == 0:
-             xd += 1
+             xd += 0.7
         else:
-             xd -= 1
-        yd = f
+             xd -= 0.7
+        yd = f*0.7
         navigate_wait(x=xd, y=yd)
 
 navigate_wait(x=0,y=0)
 land()
+
 
